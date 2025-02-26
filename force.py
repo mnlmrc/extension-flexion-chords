8b191033deb09cb18158c8119e9ccced5d95ff40
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2025-02-26 12:03:49 -0500

    update force single trial

--- force.py
+++ force.py
@@ -111,13 +111,13 @@ def calc_segment(x, hold_time=gl.hold_time, chordID=None):
     return x_s, starttime, endtime
 
 
-def calc_avg_force(experiment=None, sn=None, session=None, blocks=None,):
-    ch_idx = [col in gl.channels['mov'] for col in gl.diffCols[experiment]]
+def calc_single_trial_metrics(experiment=None, sn=None, session=None, day=None, blocks=None):
+    ch_idx = np.array(gl.diffCols[experiment])
 
-    dat = pd.read_csv(os.path.join(gl.baseDir, experiment, session, f'subj{sn}', f'{experiment}_{sn}.dat'),
+    dat = pd.read_csv(os.path.join(gl.baseDir, experiment, session, f'day{day}', f'subj{sn}',f'{experiment}_{sn}.dat'),
                       sep='\t')
 
-    force_dict = {
+    single_trial_metrics = {
         'subNum': [],
         'BN': [],
         'TN': [],
@@ -126,61 +126,80 @@ def calc_avg_force(experiment=None, sn=None, session=None, blocks=None,):
         'middle': [],
         'ring': [],
         'pinkie': [],
+        'thumb_der': [],
+        'index_der': [],
+        'middle_der': [],
+        'ring_der': [],
+        'pinkie_der': [],
         'trialPoint': [],
         'RT': [],
         'ET': [],
-        'MD': []
+        'MD': [],
+        'MD_c++': [],
+        'chordID': []
+
     }
     for bl in blocks:
 
         dat_tmp = dat[dat['BN'] == int(bl)]
 
-        filename = os.path.join(gl.baseDir, experiment, session, f'subj{sn}', f'{experiment}_{sn}_{int(bl):02d}.mov')
+        filename = os.path.join(gl.baseDir, experiment, session, f'day{day}', f'subj{sn}', f'{experiment}_{sn}_{int(bl):02d}.mov')
 
         mov = load_mov(filename)
         mov = np.concatenate(mov, axis=0)
 
         mov = mov[mov[:, 1] == gl.wait_exec[experiment]]
 
-        print(f'Processing... subj{sn}, block {bl}, {mov[:, 0].max()} trials found...')
+        ntrials = int(mov[:, 0].max())
+
+        print(f'Processing... subj{sn}, block {bl}, {ntrials} trials found...')
 
         assert (mov[:, 0].max() == len(dat_tmp))
 
-        for ntrial in range(mov[:, 0].max()):
-            force_tmp = mov[mov[:, 0] == ntrial + 1, ch_idx]
+        for ntrial in range(ntrials):
+
+            force_tmp = mov[mov[:, 0] == ntrial + 1][:, ch_idx]
 
             force_filt = lowpass_fir(force_tmp, n_ord=4, cutoff=10, fsample=gl.fsample['force'], axis=0)
             force_der1 = np.gradient(force_filt, 1 / gl.fsample['force'], axis=0)
 
-            force_der1_avg = force_der1.mean(axis=0).abs()
+            force_der1_avg = np.abs(force_der1.mean(axis=0))
 
-            rt_samples = dat_tmp.iloc[ntrial]['RT']
-            et_samples = rt_samples + dat_tmp.iloc[ntrial]['ET']
+            if dat_tmp.iloc[ntrial].trialPoint == 1:
+                rt = dat_tmp.iloc[ntrial]['RT'] / 1000
+                et = dat_tmp.iloc[ntrial]['ET'] / 1000
+                rt_samples = int(rt * gl.fsample['force'])
+                et_samples = rt_samples + int( et * gl.fsample['force'])
+            else:
+                rt_samples = 0
+                et_samples = len(force_tmp) - int(gl.hold_time * gl.fsample['force'])
             
-            MD = calc_md(force_tmp[rt_samples:et_samples])
+            MD, _ = calc_md(force_tmp[rt_samples:et_samples])
             force_avg = force_tmp[-et_samples:].mean(axis=0)
 
-            force_dict['thumb'].append(force_avg[0])
-            force_dict['index'].append(force_avg[1])
-            force_dict['middle'].append(force_avg[2])
-            force_dict['ring'].append(force_avg[3])
-            force_dict['pinkie'].append(force_avg[4])
-            force_dict['thumb_der'].append(force_der1_avg[0])
-            force_dict['index_der'].append(force_der1_avg[1])
-            force_dict['middle_der'].append(force_der1_avg[2])
-            force_dict['ring_der'].append(force_der1_avg[3])
-            force_dict['pinkie_der'].append(force_der1_avg[4])
-            force_dict['RT'].append(dat_tmp.iloc[ntrial]['RT'])
-            force_dict['ET'].append(dat_tmp.iloc[ntrial]['ET'])
-            force_dict['MD'].append(MD)
-            force_dict['MD_c++'].append(dat_tmp.iloc[ntrial]['MD'])
-            force_dict['BN'].append(dat_tmp.iloc[ntrial]['BN'])
-            force_dict['TN'].append(dat_tmp.iloc[ntrial]['TN'])
-            force_dict['trialPoint'].append(dat_tmp.iloc[ntrial]['trialPoint'])
+            single_trial_metrics['subNum'].append(dat_tmp.iloc[ntrial]['subNum'])
+            single_trial_metrics['chordID'].append(dat_tmp.iloc[ntrial]['chordID'])
+            single_trial_metrics['thumb'].append(force_avg[0])
+            single_trial_metrics['index'].append(force_avg[1])
+            single_trial_metrics['middle'].append(force_avg[2])
+            single_trial_metrics['ring'].append(force_avg[3])
+            single_trial_metrics['pinkie'].append(force_avg[4])
+            single_trial_metrics['thumb_der'].append(force_der1_avg[0])
+            single_trial_metrics['index_der'].append(force_der1_avg[1])
+            single_trial_metrics['middle_der'].append(force_der1_avg[2])
+            single_trial_metrics['ring_der'].append(force_der1_avg[3])
+            single_trial_metrics['pinkie_der'].append(force_der1_avg[4])
+            single_trial_metrics['RT'].append(dat_tmp.iloc[ntrial]['RT'])
+            single_trial_metrics['ET'].append(dat_tmp.iloc[ntrial]['ET'])
+            single_trial_metrics['MD'].append(MD)
+            single_trial_metrics['MD_c++'].append(dat_tmp.iloc[ntrial]['MD'])
+            single_trial_metrics['BN'].append(dat_tmp.iloc[ntrial]['BN'])
+            single_trial_metrics['TN'].append(dat_tmp.iloc[ntrial]['TN'])
+            single_trial_metrics['trialPoint'].append(dat_tmp.iloc[ntrial]['trialPoint'])
 
-    force_df = pd.DataFrame(force_dict)
+    single_trial_metrics = pd.DataFrame(single_trial_metrics)
 
-    return force_df
+    return single_trial_metrics
 
 
 def main():
@@ -188,118 +207,17 @@ def main():
 
     parser.add_argument('what', nargs='?', default=None)
     parser.add_argument('--experiment', type=str, default=None)
+    parser.add_argument('--session', type=str, default=None)
     parser.add_argument('--sn', type=int, default=None)
     parser.add_argument('--day', type=int, default=None)
+    parser.add_argument('--blocks', type=int, default=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
     args = parser.parse_args()
 
     if args.what == 'single_trial':
 
-        behavioural_dict = {
-            'BN': [],
-            'TN': [],
-            'participant_id': [],
-            'subNum': [],
-            'chordID': [],
-            'chord': [],
-            'trialPoint': [],
-            'repetition': [],
-            'day': [],
-            'MD': [],
-            'RT': [],
-            'ET': [],
-            'thumb_force': [],
-            'index_force': [],
-            'middle_force': [],
-            'ring_force': [],
-            'pinkie_force': [],
-        }
-
-        path = os.path.join(gl.baseDir, args.experiment, gl.behavDir, f"day{args.day}", f'subj{args.sn}')
-
-        pinfo = pd.read_csv(os.path.join(gl.baseDir, args.experiment, 'participants.tsv'), sep='\t')
-        blocks = pinfo[pinfo.sn == args.sn].FuncRuns[0].split('.')
-
-        dat = pd.read_csv(os.path.join(path, f"{args.experiment}_{args.sn}.dat"), sep="\t")
-
-        for block in blocks:
-
-            block = int(block)
-
-            print(f"experiment:{args.experiment}, "
-                  f"participant_id:subj{args.sn}, "
-                  f"day:{args.day}, "
-                  f"block:{block}")
-
-            filename = os.path.join(path, f'{args.experiment}_{args.sn}_{block:02d}.mov')
-
-            mov = load_mov(filename)
-
-            dat_tmp = dat[dat.BN == block].reset_index()  # .dat file for block
-
-            for tr in range(len(mov)):
-
-                if tr == 0 or dat_tmp.iloc[tr].chordID != dat_tmp.iloc[tr - 1].chordID:
-                    rep = 1
-                else:
-                    rep += 1
-
-                chordID = dat_tmp.iloc[tr].chordID.astype(int).astype(str)
-                chord = 'trained' if chordID in pinfo[pinfo['sn'] == args.sn]['trained'][0].split('.') else 'untrained'
-
-                # add trial info to dictionary
-                behavioural_dict['BN'].append(dat_tmp.iloc[tr].BN)
-                behavioural_dict['TN'].append(dat_tmp.iloc[tr].TN)
-                behavioural_dict['subNum'].append(args.sn)
-                behavioural_dict['participant_id'].append(f'subj{args.sn}')
-                behavioural_dict['chordID'].append(chordID)
-                behavioural_dict['trialPoint'].append(dat_tmp.iloc[tr].trialPoint)
-                behavioural_dict['chord'].append(chord)
-                behavioural_dict['day'].append(args.day)
-                behavioural_dict['repetition'].append(rep)
-
-                if dat_tmp.iloc[tr].trialPoint == 1:
-
-                    forceRaw = mov[tr][:, gl.diffCols][
-                                   mov[tr][:, 1] == 4] * gl.fGain  # take only states 3 (i.e., WAIT_EXEC)
-
-                    # calc single trial metrics
-                    force, rt, et = calc_segment(forceRaw, hold_time=None, chordID=dat_tmp.iloc[tr].chordID)
-
-                    force_avg = force.mean(axis=0)
-
-                    assert rt > 0, "negative reaction time"
-                    # if et is not None:
-                    assert et > 0, "negative execution time"
-                    # else:
-                    #     pass
-
-                    md, _ = calc_md(force)
-
-                    # add measures to dictionary
-                    behavioural_dict['RT'].append(rt)
-                    behavioural_dict['ET'].append(et)
-                    behavioural_dict['MD'].append(md)
-                    behavioural_dict['thumb_force'].append(force_avg[0])
-                    behavioural_dict['index_force'].append(force_avg[1])
-                    behavioural_dict['middle_force'].append(force_avg[2])
-                    behavioural_dict['ring_force'].append(force_avg[3])
-                    behavioural_dict['pinkie_force'].append(force_avg[4])
-
-                else:
-
-                    # add to dictionary
-                    behavioural_dict['RT'].append(None)
-                    behavioural_dict['ET'].append(None)
-                    behavioural_dict['MD'].append(None)
-                    behavioural_dict['thumb_force'].append(None)
-                    behavioural_dict['index_force'].append(None)
-                    behavioural_dict['middle_force'].append(None)
-                    behavioural_dict['ring_force'].append(None)
-                    behavioural_dict['pinkie_force'].append(None)
-
-        behavioural = pd.DataFrame(behavioural_dict)
-        behavioural.to_csv(os.path.join(gl.baseDir, args.experiment, gl.behavDir, f'day{args.day}', f'subj{args.sn}',
+        single_trial_metrics = calc_single_trial_metrics(args.experiment, args.sn, args.session, args.day, args.blocks)
+        single_trial_metrics.to_csv(os.path.join(gl.baseDir, args.experiment, args.session, f'day{args.day}', f'subj{args.sn}',
                                         'single_trial.tsv'), sep='\t', index=False)
 
 
