90cda6b673f521202794abf477535e44654ae4b8
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2025-03-19 17:46:45 -0400

    Update makeTGT.py

--- efc4/makeTGT.py
+++ efc4/makeTGT.py
@@ -2,59 +2,75 @@ import os
 
 import pandas as pd
 import numpy as np
+import globals as gl
 
 experiment = 'efc4'
-sn = 1001
-day = 2
+sn = 101
+# day = 2
 
-session = 'testing'
+pinfo = pd.read_table(os.path.join(gl.baseDir, experiment, 'participants.tsv'), sep='\t')
+pinfo_row = pinfo[pinfo.sn == sn].reset_index(drop=True)
+trained = np.array(pinfo_row['trained'][0].split('.'), dtype='int')
 
-nruns = 10
+sess_info = pd.read_csv('target/sess_info.tgt', delimiter='\t')
 
-file_path = 'target/template.tgt'
-out_path = 'target/'
-data = pd.read_csv(file_path, delimiter='\t')
+for i, row in sess_info.iterrows():
 
-for n in range(nruns):
+    day = row['day']
+    week = row['week']
+    nruns = row['nruns']
+    session = row['session']
+    miniblocks = row['miniblocks']
 
-    target = pd.DataFrame()
-    for mblock in range(3):
-        # preserved_columns = data[['startTime', 'endTime']]
+    # nruns = 10
 
-        # Shuffle the order of chordID groups first
-        unique_chord_ids = data['chordID'].unique()  # data.drop(columns=['startTime', 'endTime'])['chordID'].unique()
-        np.random.shuffle(unique_chord_ids)
+    file_path = 'target/template.tgt'
+    out_path = 'target/'
+    target_template = pd.read_csv(file_path, delimiter='\t')
 
-        # Shuffling rows within each 'chordID' group and combining them in a shuffled order
-        shuffled_groups = [
-            data[data['chordID'] == chord_id].sample(frac=1).reset_index(drop=True)
-            for chord_id in unique_chord_ids
-        ]
-        target_tmp = pd.concat(shuffled_groups).reset_index(drop=True)
-        # target_tmp[['startTime', 'endTime']] = preserved_columns
+    for n in range(nruns):
 
-        target = pd.concat([target, target_tmp]).reset_index(drop=True)
+        target = pd.DataFrame()
+        for mblock in range(miniblocks):
 
-    start = 6000
-    step = 6000
-    num = target.shape[0]
+            # Shuffle the order of chordID groups first
+            unique_chord_ids = target_template['chordID'].unique()
+            if session == 'training':
+                unique_chord_ids = unique_chord_ids[np.isin(unique_chord_ids, trained)]
+            np.random.shuffle(unique_chord_ids)
 
-    startTime = start + step * np.arange(num)
-    startTime[16:] = startTime[16:] + 16000
-    startTime[32:] = startTime[32:] + 16000
-    endTime = np.zeros_like(startTime)
-    endTime[-1] = startTime[-1] + target[['planTime', 'execMaxTime', 'feedbackTime', 'iti']].iloc[-1].sum() + 10500
+            # Shuffling rows within each 'chordID' group and combining them in a shuffled order
+            shuffled_groups = [
+                target_template[target_template['chordID'] == chord_id].sample(frac=1).reset_index(drop=True)
+                for chord_id in unique_chord_ids
+            ]
+            target_tmp = pd.concat(shuffled_groups).reset_index(drop=True)
+            # target_tmp[['startTime', 'endTime']] = preserved_columns
 
-    # # Shuffle 'iti' and 'Trig' columns independently
-    # target['iti'] = np.random.permutation(target['iti'].values)
-    # target['Trig'] = np.random.permutation(target['Trig'].values)
-    # target['stimTrig'] = np.random.permutation(target['stimTrig'].values)
-    target['startTime'] = startTime
-    target['endTime'] = endTime
+            target = pd.concat([target, target_tmp]).reset_index(drop=True)
 
-    target['subNum'] = sn
+        start = 6000
+        step = 6000
+        num = target.shape[0]
 
-    target.to_csv(os.path.join(out_path, f'{experiment}_{session}_{sn}_day{day}_run{n+1}.tgt'), index=False, sep='\t')
+        startTime = start + step * np.arange(num)
+        if session == 'scanning':  # add breaks for baseline recordings if it is a scanning session
+            startTime[16:] = startTime[16:] + 16000
+            startTime[32:] = startTime[32:] + 16000
+        endTime = np.zeros_like(startTime)
+        if session == 'scanning':
+            endTime[-1] = startTime[-1] + target[['planTime', 'execMaxTime', 'feedbackTime', 'iti']].iloc[-1].sum() + 10500
+
+        target['startTime'] = startTime
+        target['endTime'] = endTime
+
+        target['session'] = session
+        target['day'] = day
+        target['week'] = week
+
+        target['subNum'] = sn
+
+        target.to_csv(os.path.join(out_path, f'{experiment}_{session}_{sn}_day{day}_run{n+1}.tgt'), index=False, sep='\t')
 
 
 
