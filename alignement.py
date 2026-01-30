b69df2c5d7e5623588af22ca152d1eb5ac4a9396
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2026-01-30 12:07:34 -0500

    Update alignement.py

--- alignement.py
+++ alignement.py
@@ -10,31 +10,23 @@ import imaging_pipelines.betas as bt
 import nitools as nt
 
 def main(args):
-    nSess = 3
-    atlas = 'ROI'
+    #nSess = 3
+    atlas = 'Hem'
     Hem = ['L', 'R']
-    struct = ['CortexLeft', 'CortexRight']
-    rois = gl.rois[atlas]
+    #struct = ['CortexLeft', 'CortexRight']
     path_glm = os.path.join(gl.baseDir, args.experiment, f'{gl.glmDir}{args.glm}', f'subj{args.sn}')
     path_rois = os.path.join(gl.baseDir, args.experiment, gl.roiDir, f'subj{args.sn}')
     path_alignment = os.path.join(gl.baseDir, args.experiment, 'alignment', f'subj{args.sn}')
     os.makedirs(path_alignment, exist_ok=True)
-    if args.what=='intercept_ROI':
+    if args.what=='intercept':
         cifti = nb.load(os.path.join(path_glm, 'intercept.dscalar.nii'))
         betas = nt.volume_from_cifti(cifti)
         coords_merged = []
         for h, H in enumerate(Hem):
-            for r, roi in enumerate(rois):
-                mask = nb.load(os.path.join(path_rois, f'{atlas}.{H}.{roi}.nii'))
-                coords = nt.get_mask_coords(mask)
-                coords_merged.append(coords)
-                X = nt.sample_image(betas, coords[0], coords[1], coords[2], interpolation=0).T
-                X = X[:, np.isnan(X).sum(axis=0)==0]
-                Xc = X - X.mean(axis=1, keepdims=True)
-                cov = Xc @ Xc.T
-                std = np.sqrt(np.diag(cov))  # vector of standard deviations
-                r = cov / np.outer(std, std)  # elementwise division
-                np.save(os.path.join(gl.baseDir, path_alignment, f'corr.intercept.{H}.{roi}.npy'), r)
+            #for r, roi in enumerate(rois):
+            mask = nb.load(os.path.join(path_rois, f'{atlas}.{H}.nii'))
+            coords = nt.get_mask_coords(mask)
+            coords_merged.append(coords)
         coords = np.concatenate(coords_merged, axis=1)
         X = nt.sample_image(betas, coords[0], coords[1], coords[2], interpolation=0).T
         X = X[:, (np.isnan(X).sum(axis=0) == 0) & (np.any(np.abs(X) > 1e-8, axis=0))]
@@ -43,11 +35,32 @@ def main(args):
         std = np.sqrt(np.diag(cov))  # vector of standard deviations
         r = cov / np.outer(std, std)  # elementwise division
         np.save(os.path.join(gl.baseDir, path_alignment, f'corr.intercept.merged.npy'), r)
-    if args.what == 'intercept_ROI_all':
+    if args.what=='intercept_SML':
+        sns = [5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+        for sn in sns:
+            path = os.path.join(gl.baseDir, args.experiment,'SML', 'glmSess1', f's{sn:02d}')
+            cifti = nb.load(os.path.join(path, 'intercept.dscalar.nii'))
+            betas = nt.volume_from_cifti(cifti)
+            coords_merged = []
+            for h, H in enumerate(Hem):
+                mask = nb.load(os.path.join(path, f'{atlas}.{H}.nii'))
+                coords = nt.get_mask_coords(mask)
+                coords_merged.append(coords)
+            coords = np.concatenate(coords_merged, axis=1)
+            X = nt.sample_image(betas, coords[0], coords[1], coords[2], interpolation=0).T
+            X = X[:, (np.isnan(X).sum(axis=0) == 0) & (np.any(np.abs(X) > 1e-8, axis=0))]
+            Xc = X - X.mean(axis=1, keepdims=True)
+            cov = Xc @ Xc.T
+            std = np.sqrt(np.diag(cov))  # vector of standard deviations
+            r = cov / np.outer(std, std)  # elementwise division
+            path_save = os.path.join(gl.baseDir,args.experiment, 'SML', 'alignment', f's{sn:02d}')
+            os.makedirs(path_save, exist_ok=True)
+            np.save(os.path.join(path_save, f'corr.intercept.merged.npy'), r)
+    if args.what == 'intercept_all':
         for sn in args.sns:
             print(f'doing participant {sn}')
             args = argparse.Namespace(
-                what='intercept_ROI',
+                what='intercept',
                 experiment=args.experiment,
                 sn=sn,
                 glm=args.glm
