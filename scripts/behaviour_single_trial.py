a584cf573e78f7fd8d3324848fe961f742c5e0e0
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2026-08-19 23:10:12 -0400

    updates

--- scripts/behaviour_single_trial.py
+++ scripts/behaviour_single_trial.py
@@ -2,7 +2,7 @@ import EFC_learningfMRI.behaviour as behav
 import EFC_learningfMRI.globals as gl
 
 if __name__=='__main__':
-    sns      = gl.participants[10:]
+    sns      = gl.participants
     # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
     sessions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 
